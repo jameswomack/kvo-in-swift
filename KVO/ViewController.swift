7f28ad3eda6a38778dac289ccc96033df0cfb588
James Womack <james.womack@am.sony.com>
2014-08-03 14:56:03 -0700

    Initial Commit

---
+++ KVO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KVO
+//
+//  Created by James Womack on 8/3/14.
+//  Copyright (c) 2014 James Womack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
